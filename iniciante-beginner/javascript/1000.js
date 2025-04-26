8b3f7513ca412bb1bb4ad856ac1323ed01a639aa
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-04-25 21:49:54 -0300

    Adiciona 1000.js

---
+++ iniciante-beginner/javascript/1000.js
@@ -0,0 +1,13 @@
+/**
+ * Source: Beecrowd
+ * 
+ * @author: Letícia Cruz
+ * @date: 2025-04-21
+ * @problem: 1000 - Hello World!
+ * 
+ */
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+console.log("Hello World!");
