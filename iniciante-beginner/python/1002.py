9a6d94d99e1dc1e526a0547c35403675fd41338b
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-05-02 19:07:30 -0300

    feat: Adiciona 1002.py

---
+++ iniciante-beginner/python/1002.py
@@ -0,0 +1,15 @@
+'''
+ * Source: Beecrowd
+ * @problem: 1002 - Area of a Circle
+ * @subject: Sequential
+ *
+ * @author: Letícia Cruz
+ * @date: 2025-05-02
+'''
+
+pi = 3.14159
+raio = float(input()) #entrada
+
+area = raio*raio*pi #calculo da area
+
+print(f'AREA={area:.4f}') #saída com 4 casas decimais
