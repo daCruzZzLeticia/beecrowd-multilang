08086c197d0b3262bbebc32c86b449b7354bc09e
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-05-02 18:41:07 -0300

    feat: Adiciona 1000.py

---
+++ iniciante-beginner/python/1000.py
@@ -0,0 +1,10 @@
+'''
+ * Source: Beecrowd
+ * @problem: 1000 - Hello World!
+ * @subject: Sequential
+ *
+ * @author: Letícia Cruz
+ * @date: 2025-04-26
+'''
+
+print("Hello World!") # imprime Hello World!
