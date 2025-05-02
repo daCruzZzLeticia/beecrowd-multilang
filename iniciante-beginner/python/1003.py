ee487d4679389430a2ec33e9712722c600f15c2c
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-05-02 19:08:07 -0300

    feat: Adiciona 1003.py

---
+++ iniciante-beginner/python/1003.py
@@ -0,0 +1,16 @@
+'''
+ * Source: Beecrowd
+ * @problem: 1003 - Simple Sum
+ * @subject: Sequential
+ *
+ * @author: Letícia Cruz
+ * @date: 2025-05-02
+'''
+
+# entradas
+A = int(input())
+B = int(input())
+
+SOMA = A+B # calculo da soma
+
+print("SOMA =", SOMA) # imprime soma
