619cf7fa90757164a19e877b7feade74ab747132
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-05-02 18:42:12 -0300

    Adiciona 1001.py

---
+++ iniciante-beginner/python/1001.py
@@ -0,0 +1,16 @@
+'''
+ * Source: Beecrowd
+ * @problem: 1001 - Extremely Basic
+ * @subject: Sequential
+ *
+ * @author: Letícia Cruz
+ * @date: 2025-04-26
+'''
+
+# entradas
+A = int(input())
+B = int(input())
+
+soma = A+B # calculo soma
+
+print("X =", soma) # imprime soma
